3a95328fa55c3fb83a60090d31cb169c26ffdda7
Brian Sperlongano <zelonewolf@gmail.com>
2021-06-27 20:39:09 -0400

    Shield rendering for interstate, us highway, us historic

---
+++ style/js/shield.js
@@ -0,0 +1,143 @@
+function loadShield(ctx, shield) {
+  var imgData = ctx.createImageData(shield.data.width, shield.data.height);
+  for (i = 0; i < shield.data.data.length; i++) {
+    imgData.data[i] = shield.data.data[i];
+  }
+  ctx.putImageData(imgData, 0, 0);
+}
+
+var shields = {};
+var shieldImages = [];
+var shieldsLoaded = false;
+
+function initShields(map) {
+  shieldImages = map.style.imageManager.images;
+
+  shields["US:I"] = {
+    backgroundImage: shieldImages.shield40_us_interstate,
+    textColor: "white",
+    font2: "bold 44pt Arial",
+    font3: "bold 30pt Arial",
+    textX: 0.5,
+    textY: 0.56,
+  };
+
+  shields["US:US"] = {
+    backgroundImage: shieldImages.shield40_us_us,
+    textColor: "black",
+    font2: "bold 44pt Arial",
+    font3: "bold 30pt Arial",
+    textX: 0.5,
+    textY: 0.56,
+  };
+
+  shields["US:US:Historic"] = {
+    backgroundImage: shieldImages.shield40_us_us,
+    textColor: "black",
+    font2: "bold 44pt Arial",
+    font3: "bold 30pt Arial",
+    textX: 0.5,
+    textY: 0.56,
+    colorLighten: "#613214",
+  };
+}
+
+function missingIconLoader(map, e) {
+  var id = e.id;
+
+  if (id == "shield_") {
+    return;
+  }
+
+  if (!shieldsLoaded) {
+    initShields(map);
+    shieldsLoaded = true;
+  }
+
+  var network_ref = id.split("_")[1];
+  var network_ref_parts = network_ref.split("=");
+  var network = network_ref_parts[0];
+  var ref = network_ref_parts[1];
+
+  var width = 20;
+  var height = 20;
+  var scaleH = 1;
+  var scaleW = 1;
+  var colorLighten = null;
+
+  var c = document.createElement("canvas");
+  var ctx = c.getContext("2d");
+  ctx.imageSmoothingQuality = "high";
+  ctx.mozImageSmoothingEnabled = true;
+  ctx.webkitImageSmoothingEnabled = true;
+  ctx.msImageSmoothingEnabled = true;
+  ctx.imageSmoothingEnabled = true;
+
+  if (shields[network] != null) {
+    var shieldDef = shields[network];
+    var shield = shieldDef.backgroundImage;
+    scaleW = shield.data.width / width;
+    scaleH = shield.data.height / height;
+    colorLighten = shieldDef.colorLighten;
+
+    loadShield(ctx, shield);
+
+    ctx.fillStyle = shieldDef.textColor;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    if (ref.length <= 2) {
+      ctx.font = shieldDef.font2;
+    } else {
+      ctx.font = shieldDef.font3;
+    }
+    ctx.fillText(
+      ref,
+      width * scaleW * shieldDef.textX,
+      height * scaleH * shieldDef.textY,
+      width * scaleW
+    );
+  } else {
+    switch (network) {
+      default: {
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, 20, 20);
+        ctx.lineWidth = 2;
+        ctx.strokeRect(0, 0, 20, 20);
+
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        if (ref.length <= 2) {
+          ctx.font = "bold 11pt Arial";
+        } else {
+          ctx.font = "bold 7pt Arial";
+        }
+        ctx.fillText(ref, width * 0.5, height * 0.58, width);
+      }
+    }
+  }
+
+  var scaleCanvas = document.createElement("canvas");
+  var scaleCtx = scaleCanvas.getContext("2d");
+  scaleCtx.scale(1 / scaleW, 1 / scaleH);
+  scaleCtx.drawImage(c, 0, 0);
+
+  console.log(network);
+  console.log(colorLighten);
+
+  if (colorLighten != null) {
+    scaleCtx.globalCompositeOperation = "lighten";
+    scaleCtx.fillStyle = colorLighten;
+    scaleCtx.fillRect(0, 0, width * scaleW, height * scaleH);
+    scaleCtx.globalCompositeOperation = "destination-atop";
+    scaleCtx.drawImage(c, 0, 0);
+  }
+
+  var imgData = scaleCtx.getImageData(0, 0, width, height);
+
+  map.addImage(id, {
+    width: width,
+    height: width,
+    data: imgData.data,
+  });
+}
